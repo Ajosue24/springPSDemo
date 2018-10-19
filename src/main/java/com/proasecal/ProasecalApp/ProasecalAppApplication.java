2857c1e67c9b77c77766123415fef685656eeaec
Alexis Josue <jvasquez@avanzamas.com.co>
2018-10-19 16:11:49 -0500

    [source] se anade conf inicial del proyecto

---
+++ src/main/java/com/proasecal/ProasecalApp/ProasecalAppApplication.java
@@ -0,0 +1,12 @@
+package com.proasecal.ProasecalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProasecalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProasecalAppApplication.class, args);
+	}
+}
